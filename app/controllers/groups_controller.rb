45e06d3ca819fa9e8a21e4fbdd01100f0ecaa89b
Shaneen Vitug <shaneenvitug@gmail.com>
2019-11-04 11:15:55 +1100

    group crud almost complete

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,44 @@
+class GroupsController < ApplicationController
+  def index
+    @groups = Group.all
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    group = Group.create group_params
+    if group.save
+      flash[:success] = "Successfully created new group"
+      redirect_to groups_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @group = Group.find params[:id]
+  end
+
+  def update
+    group = Group.find params[:id]
+    group.update group_params
+    redirect_to group
+  end
+
+  def show
+    @group = Group.find params[:id]
+  end
+
+  def destroy
+    group = Group.find params[:id]
+    group.destroy
+    redirect_to groups_path
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # resources is only for CRUD operations
   resources :users, :only => [:new, :create] # same as - :except => [:destroy]
   resources :contacts
+  resources :groups
   # login is not CRUD
   get '/login' => 'session#new' # This will be our sign-in page.
   post '/login' => 'session#create' # This will be the path to which the sign-in form is posted
