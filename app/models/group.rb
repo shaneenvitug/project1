266b088b4034665a3774af62e84cb6bbae3b0981
Shaneen Vitug <shaneenvitug@gmail.com>
2019-12-26 23:09:51 +0800

    fixed backend validation for groups

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,7 @@
 class Group < ApplicationRecord
   has_and_belongs_to_many :contacts
   belongs_to :user, :optional => true
+  validates_uniqueness_of :name, scope: :user_id    #scope allows for different users to have a group called "work"
+                                                    #but each user can only have ONE group called "work"  
+  validates :name, :presence => true                #backend validation - no empty string 
 end
